2134c1ad3bfebf1fde34af60f5c825a56d6d0d03
JonathanGab <jogaborit@hotmail.com>
2021-04-12 17:46:22 +0200

    create toolbar with drawer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,76 +1,211 @@
+import React from 'react';
 import './Header.css';
-// import ReactDOM from 'react-dom';
-
-import {
-  makeStyles,
-  ThemeProvider,
-  createMuiTheme,
-} from '@material-ui/core/styles';
-import { green } from '@material-ui/core/colors';
+import clsx from 'clsx';
 import { NavLink } from 'react-router-dom';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import {
+  Drawer,
+  AppBar,
+  Toolbar,
+  List,
+  CssBaseline,
+  Typography,
+} from '@material-ui/core';
+import {
+  Divider,
+  IconButton,
+  ListItemText,
+  ListItemIcon,
+  ListItem,
+} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import HomeIcon from '@material-ui/icons/Home';
+import MovieIcon from '@material-ui/icons/Movie';
+import MailIcon from '@material-ui/icons/Mail';
+import StarsIcon from '@material-ui/icons/Stars';
+import StyleIcon from '@material-ui/icons/Style';
+const drawerWidth = 200;
 
 const useStyles = makeStyles((theme) => ({
-  navBar: {
-    display: 'inline-flex',
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9) + 1,
+    },
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
     justifyContent: 'flex-end',
-    flexGrow: 2,
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
   },
 }));
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: green[400],
-    },
-    secondary: {
-      main: '#f57c00',
-    },
-  },
-});
-
 export default function Header() {
   const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <ThemeProvider theme={theme}>
-      <div>
-        <AppBar color="secondary" position="static">
-          <Toolbar>
-            <IconButton
-              style={{ color: 'white' }}
-              className={classes.menuButton}
-              aria-label="menu"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.navBar}>
-              <nav>
-                <ul>
-                  <li>
-                    <NavLink exact activeClassName="active" to="/">
-                      Home
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink exact activeClassName="active" to="/movies">
-                      Movies
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink exact activeClassName="active" to="/favorites">
-                      Favorites
-                    </NavLink>
-                  </li>
-                </ul>
-              </nav>
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </div>
-    </ThemeProvider>
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        style={{ backgroundColor: 'orange' }}
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: open,
+            })}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Dollix
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
+        }}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          <NavLink exact activeClassName="active" to="/">
+            <ListItem button>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItem>
+          </NavLink>
+        </List>
+        <List>
+          <NavLink exact activeClassName="active" to="/movies">
+            <ListItem button>
+              <ListItemIcon>
+                <MovieIcon />
+              </ListItemIcon>
+              <ListItemText primary="Movie" />
+            </ListItem>
+          </NavLink>
+        </List>
+
+        <List>
+          <NavLink exact activeClassName="active" to="/favorite">
+            <ListItem button>
+              <ListItemIcon>
+                <StarsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Favorites" />
+            </ListItem>
+          </NavLink>
+        </List>
+
+        <Divider />
+        <List>
+          <ListItem button>
+            <ListItemIcon>
+              <StyleIcon />
+            </ListItemIcon>
+            <ListItemText primary="Categories" />
+          </ListItem>
+        </List>
+        <List>
+          <ListItem button>
+            <ListItemIcon>
+              <MailIcon />
+            </ListItemIcon>
+            <ListItemText primary="Contact Us" />
+          </ListItem>
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+      </main>
+    </div>
   );
 }
