7c31795c46cdd0c04f259b1b7f92a393e1fe6f95
Edouard Misset <edouardmisset@gmail.com>
2021-04-12 22:08:11 +0200

    cleaned up using destructuring

--- src/components/CardMovies.js
+++ src/components/CardMovies.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './CardMovies.css';
 
-function CardMovies(props) {
+function CardMovies({img, date, title, genre}) {
   return (
     <div className="Card">
-      <img className="cover" src={props.img} alt={props.img} />
-      <h4 className="title">{props.title}</h4>
-      <h5 className="date">{props.date}</h5>
-      <h5 className="gender">{props.gender}</h5>
+      <img className="cover" src={img} alt={title} />
+      <h4 className="title">{title}</h4>
+      <h5 className="date">{date}</h5>
+      <h5 className="gender">{genre}</h5>
     </div>
   );
 }
