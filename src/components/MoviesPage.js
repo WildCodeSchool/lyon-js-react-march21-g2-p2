b140b2ea3d10cba3f5847ec734c122c042bd3760
Edouard Misset <edouardmisset@gmail.com>
2021-04-12 17:37:34 +0200

    Added a button to the icon

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -2,10 +2,16 @@ import './MoviesPage.css';
 import FilterListTwoToneIcon from '@material-ui/icons/FilterListTwoTone';
 
 export default function MoviesPage() {
+  function handleFilter() {
+    // Toggle the criteria list
+    console.log('toggle');
+  }
   return (
-    <div>
-      <FilterListTwoToneIcon />
-      Movie list
-    </div>
+    <>
+      <h3 className="page-title">Movie list</h3>
+      <button type="button" onClick={handleFilter}>
+        <FilterListTwoToneIcon className="filter-icon" />
+      </button>
+    </>
   );
 }
