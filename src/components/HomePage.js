0068184316b584e4a187c3c124ffdd8d70ef7376
Heranca <heranca.kamalo@gmail.com>
2021-04-12 19:19:03 +0200

    add popular movie in home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,30 +1,16 @@
 import './HomePage.css';
-/*import Card from './card';
-import axios from 'axios';
-import React, { useState } from 'react';
+import Card from './card';
 
-const getPopularMovies = () => {
-  axios
 
-    .get(
-      'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=f22eb05a70b166bd4e2c1312e15d8e8b'
-    )
-
-    .then((response) => response.data)
-
-    .then((data) => {
-      setPopularMovies(data.results[0]);
-    });
-};
-
-const [popularMovies, setPopularMovies] = useState('');
 
 export default function HomePage() {
+  
+ 
   return (
     <>
       <div>Welcome Home</div>
-      <Card movie={popularMovies} />
+      <Card />
     </>
   );
 }
-*/
+
--- src/components/card.js
+++ src/components/card.js
@@ -1,10 +1,32 @@
 import './card.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 
 export default function Card(props) {
+  
+  const [popularMovie, setPopularMovie] = useState([]);
+
+  useEffect(() => {
+    axios
+  
+      .get(
+        'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=f22eb05a70b166bd4e2c1312e15d8e8b'
+      )
+  
+      .then((response) => response.data)
+        
+      .then((data) => {
+        setPopularMovie(data.results);
+      });
+  }, []);
+  
+  
+
   return (
-    <div className="card">
-      <img src="" alt="cover" className="cover" />
-      <h4 className="title">{props.popularMovies.title}</h4>
-    </div>
+    <ul className="card">
+      {popularMovie
+      .map(movie =>  <li>{movie.title}</li>)}
+    </ul>
   );
 }
