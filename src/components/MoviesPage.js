18ad7cc3ade63909494e1da66dce38cf3bb7d26e
Edouard Misset <edouardmisset@gmail.com>
2021-04-12 17:44:41 +0200

    added a small movie list

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,7 +1,15 @@
 import './MoviesPage.css';
+import { useState } from 'react';
 import FilterListTwoToneIcon from '@material-ui/icons/FilterListTwoTone';
 
 export default function MoviesPage() {
+  const [movieList, setMovieList] = useState([
+    { title: 'Avatar', genre: 'science-fiction', date: 2009 },
+    { title: 'Black Swan', genre: 'drama', date: 2010 },
+    { title: 'Inception', genre: 'science-fiction', date: 2010 },
+    { title: 'Moonlight', genre: 'drama', date: 2016 },
+  ]);
+
   function handleFilter() {
     // Toggle the criteria list
     console.log('toggle');
