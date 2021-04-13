465514154f9a97e6a4a85051c2b8ce2838c131bc
Doriane <berdalado@gmail.com>
2021-04-13 19:02:01 +0200

    layout card column

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -9,33 +9,56 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import IconButton from '@material-ui/core/IconButton';
-//import Grid from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
+  gridContainer: {
+    paddingLeft: '20px',
+    paddingRight: '20px',
+    minWidth: 200,
+    display: 'flex',
+    direction: 'row',
+    justify: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  Card: {
+    width: 200,
+    boxShadow: '0 5px 8px 0 rgba(0, 0, 0, 0.3)',
+  },
+  Media: {
+    height: 300,
   },
 });
 
-export default function MovieCard({ title, img }) {
+export default function MovieCard({ title, img, id }) {
   const classes = useStyles();
-
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia component="img" alt={title} height="200" image={img} />
-        <CardContent>
-          <Typography gutterBottom variant="subtitle2" component="h2">
-            {title}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-      </CardActions>
-    </Card>
+    <div>
+      <Grid container spacing={3} className={classes.gridContainer}>
+        <Grid item xs={6} sm={3} md={2} key={id}>
+          <Card className={classes.Card}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.Media}
+                component="img"
+                alt={title}
+                height="200"
+                image={img}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="subtitle2" component="h2">
+                  {title}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <IconButton aria-label="add to favorites">
+                <FavoriteIcon />
+              </IconButton>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
-//export default MovieCard;
--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -6,12 +6,14 @@ const movies = [
     title: "Le Monde de Narnia : L'Odyssée du Passeur d'aurore",
     date: 2010,
     genre: 'Fantasy',
+    id: 1,
     img: 'https://fr.web.img2.acsta.net/medias/nmedia/18/35/53/32/18463695.jpg',
   },
   {
     title: 'Le monde Narnia',
     date: 2008,
     genre: 'Fantasy',
+    id: 2,
     img:
       'https://img-4.linternaute.com/g6QIa_CMEJR52Oyy_04PVEx6sas=/1240x/85e65e8e98d2486cb19253c16001601a/ccmcms-linternaute/80965.jpg',
   },
@@ -20,6 +22,7 @@ const movies = [
       "Le Monde de Narnia : Le Lion, la Sorcière blanche et l'Armoire magique",
     date: 2005,
     genre: 'Fantasy',
+    id: 3,
     img:
       'https://img-4.linternaute.com/g6QIa_CMEJR52Oyy_04PVEx6sas=/1240x/85e65e8e98d2486cb19253c16001601a/ccmcms-linternaute/80965.jpg',
   },
@@ -28,12 +31,14 @@ const movies = [
     title: 'Lion',
     date: 2016,
     genre: 'Drame',
+    id: 4,
     img: 'https://fr.web.img3.acsta.net/pictures/17/01/25/09/36/363964.jpg',
   },
   {
     title: 'Comic Scrip',
     date: 2021,
     genre: 'Horror',
+    id: 5,
     img: 'https://image.freepik.com/vecteurs-libre/monstre-jaune_6460-403.jpg',
   },
 ];
