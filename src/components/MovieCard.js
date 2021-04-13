0abf22ea84a34fd9c69a7666f88be08169f9ad95
Doriane <berdalado@gmail.com>
2021-04-13 17:38:04 +0200

    first layout MovieCard: add title and favorite icon

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import './MovieCard.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import IconButton from '@material-ui/core/IconButton';
+//import Grid from '@material-ui/core';
 
-function MovieCard({img, date, title, genre}) {
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
+
+export default function MovieCard({ title, img }) {
+  const classes = useStyles();
+
   return (
-    <div className="Card">
-      <img className="cover" src={img} alt={title} />
-      <h4 className="title">{title}</h4>
-      <h5 className="date">{date}</h5>
-      <h5 className="genre">{genre}</h5>
-    </div>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia component="img" alt={title} height="200" image={img} />
+        <CardContent>
+          <Typography gutterBottom variant="subtitle2" component="h2">
+            {title}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
   );
 }
-
-export default MovieCard;
+//export default MovieCard;
