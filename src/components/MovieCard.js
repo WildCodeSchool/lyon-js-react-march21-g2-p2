5ce9c20a8274936b0d946ea5e796a0a6885e51c2
Doriane <berdalado@gmail.com>
2021-04-13 19:11:45 +0200

    layout: add wrap and container component

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './MovieCard.css';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
 import FavoriteIcon from '@material-ui/icons/Favorite';
-import IconButton from '@material-ui/core/IconButton';
-import Grid from '@material-ui/core/Grid';
+import {
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+  IconButton,
+  Grid,
+  Container,
+} from '@material-ui/core';
 
 const useStyles = makeStyles({
   gridContainer: {
@@ -34,31 +37,33 @@ export default function MovieCard({ title, img, id }) {
   const classes = useStyles();
   return (
     <div>
-      <Grid container spacing={3} className={classes.gridContainer}>
-        <Grid item xs={6} sm={3} md={2} key={id}>
-          <Card className={classes.Card}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.Media}
-                component="img"
-                alt={title}
-                height="200"
-                image={img}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="subtitle2" component="h2">
-                  {title}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-            </CardActions>
-          </Card>
+      <Container>
+        <Grid container spacing={3} className={classes.gridContainer}>
+          <Grid item xs={6} sm={3} md={2} key={id}>
+            <Card className={classes.Card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.Media}
+                  component="img"
+                  alt={title}
+                  height="200"
+                  image={img}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="subtitle2" component="h2">
+                    {title}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <IconButton aria-label="add to favorites">
+                  <FavoriteIcon />
+                </IconButton>
+              </CardActions>
+            </Card>
+          </Grid>
         </Grid>
-      </Grid>
+      </Container>
     </div>
   );
 }
