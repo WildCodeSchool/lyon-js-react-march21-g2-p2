e5d64f896429b8cadcf40714879e5dbd2cb4dd31
Doriane <berdalado@gmail.com>
2021-04-16 16:06:06 +0200

    MoviePage

---
+++ src/screens/MoviePage.js
@@ -0,0 +1,23 @@
+/*MoviePage function which wil call MovieList rendering in MovieCard*/
+
+/*component import*/
+import React from 'react';
+import './MoviesPage.css';
+import MovieList from './components/MovieList';
+//import MovieCard from './components/MovieCard';
+//<MovieCard /> {/*not sure how to put render MovieList with MovieCard*/}
+
+const MoviesPage = () => {
+  return (
+    <div>
+      <h1>Here is the list of all movies</h1>
+      {/* pass the const GeneralData as props to MovieList component, like
+      <MovieList generalData={generalData}/> */}
+      <MovieList>
+        {/*not sure how to put render MovieList with MovieCard*/}
+      </MovieList>
+    </div>
+  );
+};
+
+export default MoviesPage;
