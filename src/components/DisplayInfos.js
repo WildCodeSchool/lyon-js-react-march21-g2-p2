798b23de545715e8f8ccb708840d5866f94e11a5
Heranca <heranca.kamalo@gmail.com>
2021-04-20 18:36:18 +0200

    eslint correction

--- src/components/DisplayInfos.js
+++ src/components/DisplayInfos.js
@@ -3,35 +3,36 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 
 export default function DisplayInfos() {
-  return (
-    <>
-      <Grid
-        container
-        spacing={8}
-        direction="row"
-        justify="flex-start"
-        alignItems="flex-start"
-      >
-        <Grid item>
-          <img
-            src="https://image.tmdb.org/t/p/w200/uuvSvLb3ntGA9B0wx2JskVDSuWi.jpg"
-            alt="moviePosterTest"
-          />
-        </Grid>
-        <Grid item></Grid>
-        <h2>Title :</h2>
-        <p>Mad Max</p>
-        <h2>Date :</h2>
-        <p></p>
-        <h2>Director :</h2>
-        <p></p>
-        <h2>Actors :</h2>
-        <p></p>
-        <h2>Gender :</h2>
-        <p></p>
-        <h2>Synopsis :</h2>
-        <p></p>
-      </Grid>
-    </>
-  );
+    return (
+        <>
+            <Grid
+                container
+                spacing={8}
+                direction="row"
+                justify="flex-start"
+                alignItems="flex-start"
+            >
+                <Grid item>
+                <img
+                    src="https://image.tmdb.org/t/p/w200/uuvSvLb3ntGA9B0wx2JskVDSuWi.jpg"
+                    alt="moviePosterTest"
+                />
+                </Grid>
+                <Grid item>
+                    <h2>Title :</h2>
+                    <p>Mad Max</p>
+                    <h2>Date :</h2>
+                    <p></p>
+                    <h2>Director :</h2>
+                    <p></p>
+                    <h2>Actors :</h2>
+                    <p></p>
+                    <h2>Gender :</h2>
+                    <p></p>
+                    <h2>Synopsis :</h2>
+                    <p></p>
+                </Grid>
+            </Grid>
+        </>
+    );
 }
