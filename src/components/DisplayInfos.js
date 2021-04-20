0771935818636630e3dabedf29d5ff5b4ec7be59
Heranca <heranca.kamalo@gmail.com>
2021-04-20 18:33:16 +0200

    add DisplayInfos part2

--- src/components/DisplayInfos.js
+++ src/components/DisplayInfos.js
@@ -18,20 +18,19 @@ export default function DisplayInfos() {
             alt="moviePosterTest"
           />
         </Grid>
-        <Grid item>
-        </Grid>
-          <h2>Title :</h2>
-          <p>Mad Max</p>
-          <h2>Date :</h2>
-          <p></p>
-          <h2>Director :</h2>
-          <p></p>
-          <h2>Actors :</h2>
-          <p></p>
-          <h2>Gender :</h2>
-          <p></p>
-          <h2>Synopsis :</h2>
-          <p></p>
+        <Grid item></Grid>
+        <h2>Title :</h2>
+        <p>Mad Max</p>
+        <h2>Date :</h2>
+        <p></p>
+        <h2>Director :</h2>
+        <p></p>
+        <h2>Actors :</h2>
+        <p></p>
+        <h2>Gender :</h2>
+        <p></p>
+        <h2>Synopsis :</h2>
+        <p></p>
       </Grid>
     </>
   );
