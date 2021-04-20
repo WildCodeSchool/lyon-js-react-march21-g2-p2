2203d9201213b2f38302f5261c2c283ddc2fd504
Doriane <berdalado@gmail.com>
2021-04-20 19:37:59 +0200

    layout input and implemente useState, next will be to link the API and find the result of search

---
+++ src/components/SearchBox.js
@@ -0,0 +1,47 @@
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import { useState } from 'react';
+
+//style of the Search input
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '30ch',
+  },
+  input: {
+    marginLeft: theme.spacing(2),
+  },
+  iconButton: {
+    padding: 10,
+  },
+}));
+
+const SearchBox = () => {
+  const classes = useStyles();
+  const [searchResults, setSearchResults] = useState([]);
+
+  return (
+    <>
+      <Paper component="form" className={classes.root}>
+        <InputBase
+          type="text"
+          className={classes.input}
+          //value={value}
+          placeholder="Search for a movie"
+          inputProps={{ 'aria-label': 'Search for a movie' }}
+          onChange={(e) => setSearchResults(e.target.value)}
+        />
+        <IconButton
+          type="submit"
+          className={classes.iconButton}
+          aria-label="search"
+        >
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+    </>
+  );
+};
+export default SearchBox;
