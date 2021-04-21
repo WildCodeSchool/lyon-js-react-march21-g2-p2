a202136f36dcc690461c6cf4cb219dd2d23ea07b
Heranca <heranca.kamalo@gmail.com>
2021-04-21 09:48:08 +0200

    use of API - test OK part2

--- src/components/DisplayInfos.js
+++ src/components/DisplayInfos.js
@@ -3,37 +3,34 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 
 export default function DisplayInfos(props) {
-    // const chosenMovie = props.movie
-    return (
-        <>
-            <Grid
-            container
-            spacing={8}
-            direction="row"
-            justify="flex-start"
-            alignItems="flex-start"
-            >
-            <Grid item>
-                <img
-                src=""
-                alt="moviePosterTest"
-                />
-            </Grid>
-            <Grid item>
-                <h2>Title :{props.currentMovie.title}</h2>
-                <p>Mad Max</p>
-                <h2>Date :</h2>
-                <p></p>
-                <h2>Director :</h2>
-                <p></p>
-                <h2>Actors :</h2>
-                <p></p>
-                <h2>Gender :</h2>
-                <p></p>
-                <h2>Synopsis :</h2>
-                <p></p>
-            </Grid>
-            </Grid>
-        </>
-    );
+  // const chosenMovie = props.movie
+  return (
+    <>
+      <Grid
+        container
+        spacing={8}
+        direction="row"
+        justify="flex-start"
+        alignItems="flex-start"
+      >
+        <Grid item>
+          <img src="" alt="moviePosterTest" />
+        </Grid>
+        <Grid item>
+          <h2>Title :{props.currentMovie.title}</h2>
+          <p>Mad Max</p>
+          <h2>Date :</h2>
+          <p></p>
+          <h2>Director :</h2>
+          <p></p>
+          <h2>Actors :</h2>
+          <p></p>
+          <h2>Gender :</h2>
+          <p></p>
+          <h2>Synopsis :</h2>
+          <p></p>
+        </Grid>
+      </Grid>
+    </>
+  );
 }
--- src/screens/MovieInfos.js
+++ src/screens/MovieInfos.js
@@ -1,31 +1,25 @@
 /*component import*/
-import DisplayInfos from '../components/DisplayInfos'
+import DisplayInfos from '../components/DisplayInfos';
 import React from 'react';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-
-
-
-
 export default function MovieInfos() {
-
   const [movie, setMovie] = useState('');
   useEffect(() => {
-  axios
-  
-    .get(
-      'https://api.themoviedb.org/3/movie/399566?api_key=f22eb05a70b166bd4e2c1312e15d8e8b&language=en-US'
-    )
-        .then((response) => {
-      setMovie(response.data)
-    });      
+    axios
+
+      .get(
+        'https://api.themoviedb.org/3/movie/399566?api_key=f22eb05a70b166bd4e2c1312e15d8e8b&language=en-US'
+      )
+      .then((response) => {
+        setMovie(response.data);
+      });
   }, []);
-  
+
   return (
-      <>
-          <DisplayInfos currentMovie={movie} />
-      </>
-  )
-  
-};
+    <>
+      <DisplayInfos currentMovie={movie} />
+    </>
+  );
+}
