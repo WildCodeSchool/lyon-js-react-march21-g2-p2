39cb2e42c1a079dc565e8ab088a21945d1e218a7
florian.J <62655839+Hydrog3n69@users.noreply.github.com>
2021-04-21 16:44:29 +0200

    modif form and delete contact.css

--- src/screens/ContactPage.js
+++ src/screens/ContactPage.js
@@ -1,79 +1,93 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import './ContactPage.css';
+//------------------ IMPORT COMPONENTS & STYLES -------------//
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { useForm } from 'react-hook-form';
+import React from 'react';
+
+//---------------------- STYLE CSS -------------------------//
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& .MuiTextField-root': {
-      marginTop: theme.spacing(8),
-      width: '25ch',
+      display: 'flex',
+      paddingTop: 12,
     },
   },
   Button: {
-    marginTop: theme.spacing(8),
-    width: '25ch',
+    padding: 10,
+    marginTop: 15,
+    width: '35ch',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
+  TextField: {
+    paddingTop: 15,
+    width: '35ch',
+    margin: 'auto',
+    display: 'flex',
+    position: 'relative',
+    
+  },
+  form: {
+    padding:18,
+    width: '40ch',
+    margin: 'auto',
+    borderRadius: '1rem',
+    background: '#E2DFDB',
+  }
 }));
+//--------------------------- FONCTION CONTACT --------------------------//
 
-function Contact() {
+export default function Contact() {
   const { register, handleSubmit } = useForm();
+  const classes = useStyles()
   const onSubmit = (data) => {
     alert(JSON.stringify(data));
   };
 
   return (
-    <div className="Contact">
-      <form className="Form" onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <TextField
-            id="filled-basic"
-            label="firstName"
-            variant="filled"
-            {...register('firstName')}
-          />
-        </div>
-
-        <div>
-          <TextField
-            id="filled"
-            label="LastName"
-            variant="filled"
-            {...register('lastName')}
-          />
-        </div>
-
-        <div>
-          <TextField
-            id="filled"
-            label="Email"
-            variant="filled"
-            type="email"
-            {...register('email')}
-          />
-        </div>
-
-        <div>
-          <TextField
-            id="filled-multiline-static"
-            label="Text here"
-            multiline
-            rows={3}
-            defaultValue=""
-            variant="filled"
-            {...register('text')}
-          />
-        </div>
-        <>
-          <Button className="SubmitButton" type="submit">
-            Send
-          </Button>
-        </>
-      </form>
-    </div>
+    <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
+      <TextField 
+        className={classes.TextField} 
+        id="filled-basic"
+        label="firstname"
+        variant="filled"
+        {...register('firstName')}
+      />
+      <TextField
+        className={classes.TextField}
+        id="filled"
+        label="Lastname"
+        variant="filled"
+        {...register('lastName')}
+      />
+      <TextField
+        className={classes.TextField}
+        id="filled"
+        label="Email"
+        variant="filled"
+        type="email"
+        {...register('email')}
+      />
+      <TextField
+        className={classes.TextField}
+        id="filled-multiline-static"
+        label="Text here"
+        multiline
+        rows={6}
+        defaultValue=""
+        variant="filled"
+        {...register('text')}
+      />
+      <Button
+        className={classes.Button} 
+        variant="outlined"
+        type="submit">Send
+      </Button>
+    </form>
   );
 }
 
-export default Contact;
+ 
