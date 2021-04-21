155d2b14997e2437ebac6674b06fe1629e6dda61
Heranca <heranca.kamalo@gmail.com>
2021-04-21 18:37:19 +0200

    use of 2 API in the same time and display infos - OK part2

--- src/components/DisplayInfos.js
+++ src/components/DisplayInfos.js
@@ -3,43 +3,45 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 
 export default function DisplayInfos(props) {
-  return (
-    <>
-      <Grid
-        container
-        spacing={8}
-        direction="row"
-        justify="flex-start"
-        alignItems="flex-start"
-      >
-        <Grid item>
-          <img src={props.poster} alt="moviePosterTest" />
+    return (
+      <>
+        <Grid
+          container
+          spacing={8}
+          direction="row"
+          justify="flex-start"
+          alignItems="flex-start"
+        >
+          <Grid item>
+            <img src={props.poster} alt="moviePosterTest" />
+          </Grid>
+          <Grid item>
+            <h2>Title :</h2>
+            <p>{props.title}</p>
+            <h2>Date :</h2>
+            <p>{props.date}</p>
+            <h2>Director :</h2>
+            <ul>
+              {props.director
+                .filter((crew) => crew.job === 'Director')
+                .map((crew) => (
+                  <li key={crew.id}>{crew.name}</li>
+                ))}
+            </ul>
+            <h2>Actors :</h2>
+            {/* <ul>
+            {props.actors.map((actor) => (
+            <li>{actor.name}</li>
+            ))}
+            </ul> */}
+            <h2>Gender :</h2>
+    
+            <h2>Synopsis :</h2>
+            <p>{props.synopsis}</p>
+          </Grid>
         </Grid>
-        <Grid item>
-          <h2>Title :</h2>
-          <p>{props.title}</p>
-          <h2>Date :</h2>
-          <p>{props.date}</p>
-          <h2>Director :</h2>
-          <ul>
-            {props.director
-              .filter((crew) => crew.job === 'Director')
-              .map((crew) => (
-                <li key={crew.id}>{crew.name}</li>
-              ))}
-          </ul>
-          <h2>Actors :</h2>
-          {/* <ul>
-          {props.actors.map((actor) => (
-          <li>{actor.name}</li>
-          ))}
-          </ul> */}
-          <h2>Gender :</h2>
+      </>
 
-          <h2>Synopsis :</h2>
-          <p>{props.synopsis}</p>
-        </Grid>
-      </Grid>
-    </>
-  );
+        
+    );
 }
