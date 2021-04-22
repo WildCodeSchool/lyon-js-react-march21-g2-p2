293df5b77f703af4e714c0ef60a7a38416cc99e2
Heranca <heranca.kamalo@gmail.com>
2021-04-22 12:57:37 +0200

    add material ui features part2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles(() => ({
   root: {
     display: 'flex',
-
   },
 }));
 
--- src/components/DisplayInfos.js
+++ src/components/DisplayInfos.js
@@ -8,83 +8,83 @@ import { useCoverCardMediaStyles } from '@mui-treasury/styles/cardMedia/cover';
 import clsx from 'clsx';
 
 const useStyles = makeStyles(() => ({
-    card: {
-      borderRadius: '1rem',
-      boxShadow: 'none',
-      position: 'relative',
-      margin: 0,
-      width: 300,
-      height: 500,
-    },
-    content: {
-
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',  
-      zIndex: 2,
-      bottom: 0,
-      width: '100%',
-      margin: 0,
-    },
-    media: {
-      width: '100%',
-      height: '100%',
-      objectFit: 'cover',
-    },
-  }));
+  card: {
+    borderRadius: '1rem',
+    boxShadow: 'none',
+    position: 'relative',
+    margin: 0,
+    width: 300,
+    height: 500,
+  },
+  content: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    zIndex: 2,
+    bottom: 0,
+    width: '100%',
+    margin: 0,
+  },
+  media: {
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+  },
+}));
 
 export default function DisplayInfos(props) {
-    const styles = useStyles();
-    const mediaStyles = useCoverCardMediaStyles({ bgPosition: 'center' });
+  const styles = useStyles();
+  const mediaStyles = useCoverCardMediaStyles({ bgPosition: 'center' });
 
-const director = props.prodCrew.filter((crew) => crew.job === 'Director');
+  const director = props.prodCrew.filter((crew) => crew.job === 'Director');
 
-const mainActors = props.actors.slice(0, 5);
-
+  const mainActors = props.actors.slice(0, 5);
 
   return (
     <>
-        <Grid
-            container
-            spacing={8}
-            direction="row"
-            justify="flex-start"
-            alignItems="center"
-        >
-            <Grid item xs={10} sm={6} md={4} lg={3} xl={2}>
-            <Card className={clsx(styles.card)}>
+      <Grid
+        container
+        spacing={8}
+        direction="row"
+        justify="flex-start"
+        alignItems="center"
+      >
+        <Grid item xs={10} sm={6} md={4} lg={3} xl={2}>
+          <Card className={clsx(styles.card)}>
             <CardMedia
-                className={clsx(styles.cardStyle)}
-                classes={mediaStyles}
-                image={props.poster}
+              className={clsx(styles.cardStyle)}
+              classes={mediaStyles}
+              image={props.poster}
             />
-            </Card>
-            </Grid>
-            <Grid item xs={10} sm={6} md={4} lg={3} xl={2}>
-                <div className={styles.content}> 
-                    <h2>Title :</h2>
-                    <p>{props.title}</p>
-                    <h2>Date :</h2>
-                    <p>{props.date}</p>
-                    <h2>Director :</h2>
-                    <div>
-                        {director.map((director => (
-                            <p key={director.id}>{director.name}</p>
-                        )))}
-                    </div>
-                    <h2>Actors :</h2>
-                        {mainActors.map((actor) => (
-                        <>
-                        <p key={actor.id}><strong>{actor.name}</strong> - {actor.character}</p>
-                        </>
-                        ))}
-                    <h2>Genres :</h2>
-
-                    <h2>Synopsis :</h2>
-                    <p>{props.synopsis}</p>
-                </div>
-            </Grid>
+          </Card>
         </Grid>
+        <Grid item xs={10} sm={6} md={4} lg={3} xl={2}>
+          <div className={styles.content}>
+            <h2>Title :</h2>
+            <p>{props.title}</p>
+            <h2>Date :</h2>
+            <p>{props.date}</p>
+            <h2>Director :</h2>
+            <div>
+              {director.map((director) => (
+                <p key={director.id}>{director.name}</p>
+              ))}
+            </div>
+            <h2>Actors :</h2>
+            {mainActors.map((actor) => (
+              <>
+                <p key={actor.id}>
+                  <strong>{actor.name}</strong> - {actor.character}
+                </p>
+              </>
+            ))}
+            <h2>Genres :</h2>
+
+            <h2>Synopsis :</h2>
+            <p>{props.synopsis}</p>
+          </div>
+        </Grid>
+      </Grid>
     </>
   );
 }
