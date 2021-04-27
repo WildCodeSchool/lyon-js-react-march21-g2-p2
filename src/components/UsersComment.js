7c70894281dc383f83eeb0d22e1ecb251ddcc24f
Heranca <heranca.kamalo@gmail.com>
2021-04-27 18:56:30 +0200

    add css on the form w/ material ui

--- src/components/UsersComment.js
+++ src/components/UsersComment.js
@@ -10,29 +10,28 @@ import axios from 'axios';
 //---------------------- STYLE CSS -------------------------//
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        '& > *': {
-            margin: theme.spacing(2),
-            width: '25ch',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-        }
-    },
-    commentSection: {
+  root: {
+    '& > *': {
+      margin: theme.spacing(2),
+      width: '25ch',
       display: 'flex',
       flexDirection: 'column',
-      marginTop: 50,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  },
+  commentSection: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: 50,
+  },
+  Button: {
+    padding: 10,
+    marginTop: 15,
+    width: '10ch',
+  },
+}));
 
-    },
-    Button: {
-      padding: 10,
-      marginTop: 15,
-      width: '10ch',
-    },
-  }));
-    
 //--------------------------- COMMENTS FUNCTION --------------------------//
 
 export default function UserCommentsSection(props) {
@@ -50,32 +49,42 @@ export default function UserCommentsSection(props) {
   return (
     <div className={classes.commentSection}>
       <h1 className={classes.Message}>Votre avis sur ce film ?</h1>
-      <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+      <form
+        className={classes.root}
+        noValidate
+        autoComplete="off"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <TextField
-        required
-        size="small"
-        color="primary"
-        className="userName"
-        label="Name"
-        id="outlined-required"
-        defaultValue=""
-        variant="outlined"
-        {...register('userName')}
+          required
+          size="small"
+          color="primary"
+          className="userName"
+          label="Name"
+          id="outlined-required"
+          defaultValue=""
+          variant="outlined"
+          {...register('userName')}
         />
         <TextField
-        required
-        multiline
-        color="primary"
-        className="comment"
-        id="outlined-required-multiline-static"
-        label="Comment"
-        rows={2}
-        defaultValue=""
-        variant="outlined"
-        {...register('comment')}
+          required
+          multiline
+          color="primary"
+          className="comment"
+          id="outlined-required-multiline-static"
+          label="Comment"
+          rows={2}
+          defaultValue=""
+          variant="outlined"
+          {...register('comment')}
         />
-        <Button color="primary" className={classes.Button} variant="outlined" type="submit"
-        endIcon={<Icon>send</Icon>}>
+        <Button
+          color="primary"
+          className={classes.Button}
+          variant="outlined"
+          type="submit"
+          endIcon={<Icon>send</Icon>}
+        >
           Send
         </Button>
       </form>
