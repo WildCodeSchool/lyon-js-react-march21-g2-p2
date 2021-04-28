5b528ea8047ef04e009745f0b2b91bd714dc4826
Doriane <berdalado@gmail.com>
2021-04-28 13:13:15 +0200

    layout of reviews without space between cards

--- src/components/DisplayReview.js
+++ src/components/DisplayReview.js
@@ -6,16 +6,17 @@ import { useEffect } from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 //---------------------- STYLE CSS -------------------------//
 
 const useStyles = makeStyles((theme) => ({
-  root: {
+  card: {
     '& > *': {
-      margin: theme.spacing(4),
+      margin: theme.spacing(1),
       display: 'flex',
       flexDirection: 'column',
-      alignItems: 'center',
+      alignItems: 'flex-start',
       justifyContent: 'center',
     },
   },
@@ -30,30 +31,44 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function DisplayReview({ id, userName, comment }) {
-  const { root, commentSection, name } = useStyles();
+const movies = [
+  {
+    name: 'Pierre',
+    comment:
+      "Le Monde de Narnia : L'Odyssée du Passeur d'auroreLe Monde de Narnia : L'Odyssée du Passeur d'auroreLe Monde de Narnia : L'Odyssée du Passeur d'auroreLe Monde de Narnia : L'Odyssée du Passeur d'auroreLe Monde de Narnia : L'Odyssée du Passeur d'auroreLe Monde de Narnia : L'Odyssée du Passeur d'auroreLe Monde de Narnia : L'Odyssée du Passeur d'auroreLe Monde de Narnia : L'Odyssée du Passeur d'aurore",
+  },
+  {
+    name: 'Thomas',
+    comment: "j'ai adoré",
+  },
+];
+export default function DisplayReview() {
+  // export default function DisplayReview({ id, userName, comment }) {
+  const { card, commentSection, name } = useStyles();
 
-  useEffect(() => {
+  /*useEffect(() => {
     //const [review, setReview ] = useState()
     axios
       .get(`http://localhost:5000/movies/${id}/reviews`)
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
-  });
+  });*/
 
   return (
     <div className={commentSection}>
       <h2>User Reviews</h2>
-      <Card className={root} variant="outlined">
-        <CardContent>
-          <Typography className={name} color="textSecondary" gutterBottom>
-            {userName}
-          </Typography>
-          <Typography variant="body2" component="p">
-            {comment}
-          </Typography>
-        </CardContent>
-      </Card>
+      {movies.map((movie) => (
+        <Card className={card} variant="outlined">
+          <CardContent>
+            <Typography className={name} color="textSecondary" gutterBottom>
+              {movie.name}
+            </Typography>
+            <Typography variant="body2" component="p">
+              {movie.comment}
+            </Typography>
+          </CardContent>
+        </Card>
+      ))}
     </div>
   );
 }
