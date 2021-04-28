36cbd6253d45156a0443d81910c793be1ae12da6
JonathanGab <jogaborit@hotmail.com>
2021-04-28 17:41:30 +0200

    add delete Alert and add modal and reset form onsbmit

--- src/screens/ContactPage.js
+++ src/screens/ContactPage.js
@@ -2,10 +2,12 @@
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import React from 'react';
 import axios from 'axios';
-
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 //---------------------- STYLE CSS -------------------------//
 
 const useStyles = makeStyles((theme) => ({
@@ -15,8 +17,9 @@ const useStyles = makeStyles((theme) => ({
       paddingTop: 12,
     },
   },
-  Message: {
-    padding: 20,
+  message: {
+    textAlign: "center",
+    paddingBottom: "50px"
   },
   form: {
     padding: 18,
@@ -25,14 +28,14 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: '1rem',
     background: '#E2DFDB',
   },
-  TextField: {
+  textField: {
     paddingTop: 15,
     width: '35ch',
     margin: 'auto',
     display: 'flex',
     position: 'relative',
   },
-  Button: {
+  button: {
     padding: 10,
     marginTop: 15,
     width: '35ch',
@@ -40,49 +43,71 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+
+  },
 }));
 //--------------------------- FONCTION CONTACT --------------------------//
 
 export default function ContactPage() {
-  const { register, handleSubmit } = useForm();
-  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const { register, handleSubmit, reset } = useForm();
+  const { message, form, textField, button, modal, paper } = useStyles();
   const onSubmit = (form) => {
-    alert('Votre demande a bien été prise en compte!');
     axios
       .post('http://localhost:5000/contact', form)
-      .then((res) => console.log(res))
+      .then((res) => reset())
       .catch((err) => console.log(err));
   };
 
   return (
     <div>
-      <h1 className={classes.Message}>
+      <h1 className={message}>
         Des conseils? Des suggestions? N'hésitez pas à nous le faire savoir !
       </h1>
       <form
-        className={classes.form}
+        className={form}
         onSubmit={handleSubmit(onSubmit)}
         method="POST"
         action="send"
       >
         <TextField
-          className={classes.TextField}
-          id="filled-basic"
-          label="firstname"
+          className={textField}
+          id="filled"
+          label="firstName"
           variant="filled"
           required="required"
           {...register('firstName')}
         />
+
+
+
         <TextField
-          className={classes.TextField}
+          className={textField}
           id="filled"
           label="Lastname"
           variant="filled"
+
           required="required"
           {...register('lastName')}
         />
         <TextField
-          className={classes.TextField}
+          className={textField}
           id="filled"
           label="Email"
           variant="filled"
@@ -91,7 +116,7 @@ export default function ContactPage() {
           {...register('email')}
         />
         <TextField
-          className={classes.TextField}
+          className={textField}
           id="filled-multiline-static"
           label="Text here"
           multiline
@@ -101,9 +126,29 @@ export default function ContactPage() {
           required="required"
           {...register('text')}
         />
-        <Button className={classes.Button} variant="outlined" type="submit">
-          Send
-        </Button>
+        <div>
+          <Button className={button} variant="outlined" type="submit" onClick={handleOpen} >
+            Send
+      </Button>
+          <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            className={modal}
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500,
+            }}
+          >
+            <Fade in={open}>
+              <div className={paper}>
+                <p>your request has been registered! </p>
+              </div>
+            </Fade>
+          </Modal>
+        </div>
       </form>
     </div>
   );
